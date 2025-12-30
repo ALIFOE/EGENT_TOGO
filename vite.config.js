7107912ce9dafca7a1efbf09766e0c4d4b4c5789
ALIFOE <alifoebaudoin228@gmail.com>
2025-12-30 17:18:23 +0000

    repo de lien

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/EGENT_TOGO/',
+  base: process.env.NODE_ENV === 'production' ? '/EGENT_TOGO/' : '/',
   plugins: [vue()],
   build: {
     // Configuration pour build optimisé
